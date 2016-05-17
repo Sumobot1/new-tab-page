c0cbb708c499b98bf1f4fd3c9bf9473e3b0a63be
Michael Peet <ubernopeavi@gmail.com>
2016-05-17 00:41:46 -0400

    intermediate push - added background page but need t clean up

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
-define(function (require) {
+define(function(require) {
     // Load any app-specific modules
     // with a relative require call,
     // like:
@@ -6,15 +6,18 @@ define(function (require) {
 
     // Load library/vendor modules using
     // full IDs, like:
-   /* var print = require('./print');*/
-   var hello = messages.getHello();
-   console.log(hello);
-   messages.printstuff(hello);
-   messages.printstuff(messages.getHello());
-   document.body.style.background = "red";
-   document.getElementById("supportUs").addEventListener("click", function(){
-   	document.getElementById("supportUs").style.background = "green";
-   });
+    /* var print = require('./print');*/
+    var hello = messages.getHello();
+    console.log(hello);
+    messages.printstuff(hello);
+    messages.printstuff(messages.getHello());
+    document.body.style.background = "red";
+    document.getElementById("supportUs").addEventListener("click", function() {
+        document.getElementById("supportUs").style.background = "green";
+        chrome.runtime.sendMessage({ greeting: "hello" }, function(response) {
+            console.log(response.farewell);
+        });
+    });
 
     //messages.printstuff(messages.getHello());
 });
