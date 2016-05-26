776d3c6129a420bafa3a7484e264495104844653
Michael Peet <ubernopeavi@gmail.com>
2016-05-25 22:12:54 -0400

    got rid of seconds time

--- js/background/timeanddate.js
+++ js/background/timeanddate.js
@@ -6,14 +6,14 @@ timeanddate.updateTime = function () {
         var currentTime = new Date();
         var hours = currentTime.getHours();
         var minutes = currentTime.getMinutes();
-        var seconds = currentTime.getSeconds();
+        //var seconds = currentTime.getSeconds();
         if (minutes < 10){
             minutes = "0" + minutes;
         }
-        if (seconds < 10){
+/*        if (seconds < 10){
             seconds = "0" + seconds;
-        }
-        var v = hours + ":" + minutes + ":" + seconds + " ";
+        }*/
+        var v = hours + ":" + minutes;  /* + ":" + seconds + " "*/
         if(hours > 11){
             v+="PM";
         } else {
