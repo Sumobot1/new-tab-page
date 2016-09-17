57c350722d65c4f2f2dc1579adeb05f68ad37084
Michael Peet <ubernopeavi@gmail.com>
2016-09-16 21:41:45 -0400

    Put in temperature, cloudy weather

--- js/content/currentWeather.js
+++ js/content/currentWeather.js
@@ -6,18 +6,23 @@ define(function () {
             console.log(currentWeather.weather);
             console.log(currentWeather.weather[0].main) //clear, descrition: clear sky
             console.log(currentWeather.main);
+            console.log(currentWeather.main.temp);
             console.log(currentWeather.name);   //Kitchener
             if (dayOrNight === "day"){
                 if (currentWeather.weather[0].main === "Clear"){
                     sImg = '<img src="img/sun.png" class="weatherIcon"/>';
+                }else if (currentWeather.weather[0].main === "Clouds"){
+                    sImg = '<img src="img/partly-cloudy.png" class="weatherIcon"/>'
                 }
             }else{
                 if (currentWeather.weather[0].main === "Clear"){
                     sImg = '<img src="img/moon.png" class="weatherIcon"/>';
+                }else if (currentWeather.weather[0].main === "Clouds"){
+                    sImg = '<img src="img/partly-cloudy-night.png" class="weatherIcon"/>';
                 }
             }
 
-            document.getElementById("currentWeather").innerHTML = sImg+'<div class="weatherText">'+currentWeather.weather[0].description+'</div>'+'<div class="weatherText">'+currentWeather.name+'</div>';
+            document.getElementById("currentWeather").innerHTML = sImg+'<div class="weatherText">'+currentWeather.weather[0].description+'</div>'+'<div class="weatherText">'+currentWeather.name+'</div>'+'<div class="weatherText">'+String(parseInt(currentWeather.main.temp-273))+'&deg;'+'</div>';
         };
      return {
      	updateWeather: updateWeather
